21c2efeb205bd57649b2047e57dee8ed5d5793ba
Nicholas Amarillo <nicholasamarillo@gmail.com>
2023-02-16 21:42:22 -0600

    finished functionality

--- js/index.js
+++ js/index.js
@@ -1,24 +1,60 @@
 const hours = document.getElementById("hour");
 const minutes = document.getElementById("min");
 const seconds = document.getElementById("sec");
-const startButton = document.querySelector("#str").addEventListener("click", startTime);
-const resetButton = document.querySelector("#rst").addEventListener("click", resetTime);
-const stopButton = document.querySelector("#stp").addEventListener("click", stopTime);
+const startButton = document.querySelector("#str");
+const resetButton = document.querySelector("#rst");
+const stopButton = document.querySelector("#stp");
+
+let hr = 0;
+let mn = 0;
+let sc = 0;
+
+var interval;
+
+startButton.onclick = function(){
+    interval = setInterval(startTime, 1000);
+}
+
+resetButton.onclick = function(){
+    clearInterval(interval);
+    resetTime();     
+}
+
+stopButton.onclick = function(){
+    clearInterval(interval);
+    stopTime();
+}
 
 function startTime(){
-    for (let i = 0; i < 60; i++){
-        setTimeout(function(){
-            seconds.innerHTML = (i);
-        },1000)
-        console.log(i)
-        
+    sc++;
+    if (sc >= 60){
+        mn++;
+        sc = 0;
     }
+
+    if (mn >= 60){
+        hr++;
+        mn = 0;
+        
+    };
+    seconds.innerHTML = (sc);
+    minutes.innerHTML = (mn);
+    console.log(sc);
 };
 
 function resetTime(){
-
+    sc = 0;
+    mn = 0;
+    hr = 0;
+    clearInterval(interval);
+    seconds.innerHTML = (sc);
+    minutes.innerHTML = (mn);
+    console.log("timer has been reset");
 };
 
 function stopTime(){
-
+    clearInterval(interval);
+    console.log("timer has stopped! Good job!")
 };
+
+
